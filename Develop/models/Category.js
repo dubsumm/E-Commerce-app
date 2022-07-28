2dc3f14abb792b52f01212a744e17f924bea719d
dubsumm <w.b.summerlin@gmail.com>
2022-07-28 18:23:43 -0400

    filled models and index relationships

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,14 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: DataTypes.STRING,
+    allowNull: false
   },
   {
     sequelize,
